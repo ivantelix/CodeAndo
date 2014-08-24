7b85979cdbfb8f7c03b41bf0f56f73057274e06e
Israel Lugo <bmkeros@gmail.com>
2014-08-24 14:54:08 -0430

    Beta

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeAndo::Application.config.secret_token = '2a7504e172b3ca171b0b87d6b03aec730dd940b1f47bf07ff7dca718f9bbc063c274b257287685f8497b3a3d54c799180ae34600d56b97c195e1b0b0418c6e1c'
